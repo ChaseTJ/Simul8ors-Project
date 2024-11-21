7015ffed90a7ab9bdf858abe2a601406c21b3dc6
Leo Kern <mleokern@gmail.com>
2024-11-21 15:25:19 -0600

    Add files via upload

---
+++ Final Project.py
@@ -0,0 +1,17 @@
+#%% Section 1: Title
+"""
+ME 396P Final Project
+Team Simul8tors: 
+    Ang Gao
+    Chase Johnson
+    Leo Kern
+"""
+#%% Section 2: Imports
+
+#%% Section 3: Creating Model Using PyWake
+
+#%% Section 4: Creating Model Using Data
+
+#%% Plotting
+
+    
